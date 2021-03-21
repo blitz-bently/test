897ed1632f904059dd6132b06a9b951f7f68efb4
bently nixon <80673027+blitz-bently@users.noreply.github.com>
2021-03-21 17:55:30 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    let user = process.env.test_var ? process.env.test_var :  "no value";
+      return res.status(200).json({ data, user: user});
+   });
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
